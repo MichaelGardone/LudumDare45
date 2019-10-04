94dca779fb5d05c2b0c00351732d9161c3b06ddf
MichaelGardone <michaelgardone@gmail.com>
2019-10-04 17:13:50 -0600

    pushing player with dash, getting started on movement

--- LD45/Assets/Scripts/PlayerController.cs
+++ LD45/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerFacing
+{
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+}
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField]
+    PlayerFacing facing = PlayerFacing.EAST;
+
+    [SerializeField]
+    float blinkDist = 20.0f;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 10, gameObject.transform.position.y + 10, 0);
+            // Screen pos of player
+            Vector2 playerScreenPos = Camera.main.WorldToViewportPoint(transform.position);
+
+            // Screen pos of mouse
+            Vector2 mouseScreenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            // Angle between two positions
+            float angle = AngleBetweenTwoPoints(mouseScreenPos, playerScreenPos);
+            
+            // East
+            if (angle >= -45.001f && angle <= 45.0f)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + blinkDist, gameObject.transform.position.y, 0);
+            }
+            // North
+            else if (angle >= 45.001f && angle <= 135.0f)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + blinkDist, 0);
+            }
+            // West
+            else if (angle >= 135.001f || angle <= -135.001f)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - blinkDist, gameObject.transform.position.y, 0);
+            }
+            // South
+            else if (angle >= -135.0f && angle <= -45.0f)
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - blinkDist, 0);
+            }
         }
+        //else
+        //{
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+
+            }
+        //}
+
     }
+    
+    float AngleBetweenTwoPoints(Vector3 left, Vector3 right)
+    {
+        return Mathf.Atan2(left.y - right.y, left.x - right.x) * Mathf.Rad2Deg;
+    }
+    
 }
