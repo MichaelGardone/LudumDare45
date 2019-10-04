633a6cfd70d8909e04a1fad5fd552cb7888a8c47
MichaelGardone <michaelgardone@gmail.com>
2019-10-04 16:27:40 -0600

    pushing to fix glitch on josh's end

---
+++ LD45/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 10, gameObject.transform.position.y + 10, 0);
+        }
+    }
+}
