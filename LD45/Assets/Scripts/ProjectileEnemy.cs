659d354f81dfa0a7c1045d969f6956d319f4538b
joshcheung21 <joshss212312@gmail.com>
2019-10-05 15:37:35 -0600

    Started Projectile Enemy

---
+++ LD45/Assets/Scripts/ProjectileEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjectileEnemy : EnemyMaster
+{
+
+    [SerializeField]
+    private float shooting_range;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health_bar = gameObject.GetComponent<Slider>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
