30364461048f891ace541e5d73e99342cb6ebec3
MichaelGardone <michaelgardone@gmail.com>
2019-10-04 18:50:03 -0600

    shooting works

---
+++ LD45/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxTimeUntilDisappear = 5.0f;
+
+    [SerializeField]
+    float currTime;
+
+    void LateUpdate()
+    {
+        currTime += Time.deltaTime;
+
+        if (currTime >= maxTimeUntilDisappear)
+            Destroy(gameObject);
+
+    }
+}
--- LD45/Assets/Scripts/PlayerController.cs
+++ LD45/Assets/Scripts/PlayerController.cs
@@ -18,9 +18,19 @@ public class PlayerController : MonoBehaviour
     PlayerFacing facing = PlayerFacing.EAST;
 
     [SerializeField]
+    PlayerFacing facing2 = PlayerFacing.EAST;
+
+    [SerializeField]
     float blinkDist = 20.0f;
 
+    [SerializeField]
+    float initialBulletVel = 20.0f;
+
+    [SerializeField]
+    GameObject bulletPref;
+
     Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,41 +40,83 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        // Screen pos of player
+        Vector2 playerScreenPos = Camera.main.WorldToViewportPoint(transform.position);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Screen pos of mouse
+        Vector2 mouseScreenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        // Angle between two positions
+        float angle = AngleBetween(mouseScreenPos, playerScreenPos);
+
+        // East
+        if (angle >= -45.001f && angle <= 45.0f)
         {
-            // Screen pos of player
-            Vector2 playerScreenPos = Camera.main.WorldToViewportPoint(transform.position);
+            if ((angle >= -30.0f && angle <= 30.0f) || (angle >= 150.0f || angle <= -150.0f))
+                facing = PlayerFacing.EAST;
+            else if (angle > 30.0f && angle < 150.0f)
+                facing = PlayerFacing.NORTH;
+            else if (angle < -30.0f && angle > -150.0f)
+                facing = PlayerFacing.SOUTH;
 
-            // Screen pos of mouse
-            Vector2 mouseScreenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            facing2 = PlayerFacing.EAST;
+        }
+        // North
+        else if (angle >= 45.001f && angle <= 135.0f)
+        {
+            if (angle >= -30.0f && angle <= 30.0f)
+                facing = PlayerFacing.EAST;
+            else if (angle >= 150.0f || angle <= -150.0f)
+                facing = PlayerFacing.WEST;
+            else if ((angle > 30.0f && angle < 150.0f) || (angle < -30.0f && angle > -150.0f))
+                facing = PlayerFacing.NORTH;
 
-            // Angle between two positions
-            float angle = AngleBetween(mouseScreenPos, playerScreenPos);
-            
-            // East
-            if (angle >= -45.001f && angle <= 45.0f)
-            {
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x + blinkDist, gameObject.transform.position.y, 0);
-            }
-            // North
-            else if (angle >= 45.001f && angle <= 135.0f)
-            {
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + blinkDist, 0);
-            }
-            // West
-            else if (angle >= 135.001f || angle <= -135.001f)
-            {
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x - blinkDist, gameObject.transform.position.y, 0);
-            }
-            // South
-            else if (angle >= -135.0f && angle <= -45.0f)
-            {
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - blinkDist, 0);
-            }
+            facing2 = PlayerFacing.NORTH;
+        }
+        // West
+        else if (angle >= 135.001f || angle <= -135.001f)
+        {
+            if ((angle >= -30.0f && angle <= 30.0f) || (angle >= 150.0f || angle <= -150.0f))
+                facing = PlayerFacing.WEST;
+            else if (angle > 30.0f && angle < 150.0f)
+                facing = PlayerFacing.NORTH;
+            else if (angle < -30.0f && angle > -150.0f)
+                facing = PlayerFacing.SOUTH;
+
+            facing2 = PlayerFacing.WEST;
+        }
+        // South
+        else if (angle >= -135.0f && angle <= -45.0f)
+        {
+            if (angle >= -30.0f && angle <= 30.0f)
+                facing = PlayerFacing.EAST;
+            else if (angle >= 150.0f || angle <= -150.0f)
+                facing = PlayerFacing.WEST;
+            else if ((angle > 30.0f && angle < 150.0f) || (angle < -30.0f && angle > -150.0f))
+                facing = PlayerFacing.SOUTH;
+
+            facing2 = PlayerFacing.SOUTH;
         }
 
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (facing2)
+            {
+                case PlayerFacing.EAST:
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x + blinkDist, gameObject.transform.position.y, 0);
+                    break;
+                case PlayerFacing.NORTH:
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + blinkDist, 0);
+                    break;
+                case PlayerFacing.SOUTH:
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - blinkDist, 0);
+                    break;
+                case PlayerFacing.WEST:
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x - blinkDist, gameObject.transform.position.y, 0);
+                    break;
+            }
+        }
+        
         float xVel = 0, yVel = 0;
 
         if (Input.GetKey(KeyCode.W))
@@ -82,6 +134,18 @@ public class PlayerController : MonoBehaviour
             xVel = 0;
         
         rb.velocity = new Vector2(xVel, yVel);
+
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dir = mouseWorldPos - new Vector2(transform.transform.position.x, transform.transform.position.y);
+            dir.Normalize();
+
+            GameObject g = Instantiate(bulletPref, transform.position, Quaternion.identity);
+            
+            g.GetComponent<Rigidbody2D>().velocity = dir * initialBulletVel;
+        }
+
     }
     
     float AngleBetween(Vector3 left, Vector3 right)
