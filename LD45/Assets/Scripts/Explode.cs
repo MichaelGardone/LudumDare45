73269214fe7bdffa8551b43e6d391aab2bdc1d2e
joshcheung21 <joshss212312@gmail.com>
2019-10-07 12:49:23 -0600

    ExplosionTesting

---
+++ LD45/Assets/Scripts/Explode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitThenDie());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(1.05f);
+        Destroy(gameObject);
+    }
+}
