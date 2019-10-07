a3084c96592fac880b326cfa15d0c3c33462f308
MichaelGardone <michaelgardone@gmail.com>
2019-10-07 04:41:54 -0600

    mouse now locked

---
+++ LD45/Assets/Scripts/Hand.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField]
+    float outerRadius = 2.5f;
+    
+    [SerializeField]
+    Transform droog;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector3 final = Vector3.zero;
+        final.z = -2;
+
+        if (mousePos.x > droog.position.x + outerRadius)
+            final.x = droog.position.x + outerRadius;
+        else if (mousePos.x < droog.position.x - outerRadius)
+            final.x = droog.position.x - outerRadius;
+        else
+            final.x = mousePos.x;
+
+        if (mousePos.y > droog.position.y + outerRadius)
+            final.y = droog.position.x + outerRadius;
+        else if(mousePos.y < droog.position.y - outerRadius)
+            final.y = droog.position.x - outerRadius;
+        else
+            final.y = mousePos.y;
+
+        transform.position = final;
+        
+    }
+
+}
