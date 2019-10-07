5c8862836f7491413a17e02102c1ba54793ee195
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-10-07 04:33:59 -0600

    title screen

---
+++ LD45/Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+    private bool starting;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        if (!starting)
+        {
+            StartCoroutine(LoadLevel1());
+        }
+    }
+
+    private IEnumerator LoadLevel1()
+    {
+        while (audioSource.volume > 0) {
+            audioSource.volume = audioSource.volume - .1f;
+            yield return new WaitForSeconds(.1f);
+        }
+        SceneManager.LoadScene(1);
+    }
+}
