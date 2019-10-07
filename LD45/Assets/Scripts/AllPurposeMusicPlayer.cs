0cdf37cfcc693153890963aaa9431dc23cbba183
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-10-07 04:43:32 -0600

    all purpose music player

---
+++ LD45/Assets/Scripts/AllPurposeMusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllPurposeMusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource AS;
+    [SerializeField] float MaxVolume;
+    [SerializeField] float FadeRate;
+    bool isFading;
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+        AS.volume = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFading && AS.volume < MaxVolume)
+        {
+            AS.volume += FadeRate * .1f * Time.deltaTime;
+        }
+    }
+}
